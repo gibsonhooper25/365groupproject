7cce46c739b9cc905505ec12fc6b209d895d2208
gibsonhooper25 <gdhooper@calpoly.edu>
2023-10-27 11:39:12 -0700

    auth and song

--- src/api/song.py
+++ src/api/song.py
@@ -1,1 +1,11 @@
+from fastapi import APIRouter, Depends
+from pydantic import BaseModel
+from src.api import auth
+import sqlalchemy
+#from src import database as db
 
+router = APIRouter(
+    prefix="/barrels",
+    tags=["barrels"],
+    dependencies=[Depends(auth.get_api_key)],
+)
