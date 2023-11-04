9bb4d20a8ed18ead197b9c2203eb94507cb041db
gibsonhooper25 <gdhooper@calpoly.edu>
2023-11-03 21:47:09 -0700

    add song to playlist

--- src/api/playlist.py
+++ src/api/playlist.py
@@ -25,8 +25,34 @@ def create_personal_playlist():
     pass
 
 @router.post("/{playlist_id}/add-song/{song_id}")
-def add_song_to_playlist():
-    pass
+def add_song_to_playlist(playlist_id: int, song_id: int):
+    try:
+        with db.engine.begin() as connection:
+            #check if the playlist exists, return if it doesn't
+            playlist_query = sqlalchemy.select(playlists.c.id).where(playlists.c.id == playlist_id)
+            pid = connection.execute(playlist_query).first()
+            if not pid:
+                return "No playlist exists for the given playlist ID"
+
+            #check if the song exists, return if it doesn't
+            song_query = sqlalchemy.select(songs.c.id).where(songs.c.id == song_id)
+            sid = connection.execute(song_query).first()
+            if not sid:
+                return "No song exists for the given song ID"
+
+            #check if the song is already in the playlist, return if it is
+            playlist_song_query = sqlalchemy.select(playlist_songs.c.song_id).where(playlist_songs.c.playlist_id == playlist_id)
+            songs_in_playlist = connection.execute(playlist_song_query)
+            for song in songs_in_playlist:
+                if song.song_id == song_id:
+                    return "Song is already in playlist"
+
+            #add entry to playlist_songs
+            insert_query = sqlalchemy.insert(playlist_songs).values(playlist_id=playlist_id, song_id=song_id)
+            connection.execute(insert_query)
+            return "Song added to playlist"
+    except DBAPIError as error:
+        print(f"Error returned: <<<{error}>>>")
 
 @router.post("/{playlist_id}/add-songs/{album_id}")
 def add_album_songs_to_playlist():
